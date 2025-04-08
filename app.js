11aa867315948a267968fad94d9c76d48f9134e3
Btac09 <brandyapaza09@gmail.com>
2025-04-08 10:19:04 -0500

    elmine los archivs

--- app.js
+++
@@ -1,5 +0,0 @@
-console.log("hola como mundo")
-console.log("el segundo objeto de prueba")
-console.log("el tercer objeto de prueba")
-console.log("el cuarto objeto de prueba")
-console.log("el quinto objeto de a2")
